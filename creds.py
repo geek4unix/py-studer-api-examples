d0826a8f9b057e03161c4141f9f062613670aafe
Neil M <geek4unix@gmail.com>
2022-11-20 23:27:59 +0000

    initial commit

---
+++ creds.py
@@ -0,0 +1,12 @@
+
+#email registered on studer-innotec portal
+email_address="your_email@your-address.com"
+
+#password from studer-innotec password
+password="Your_Password"
+
+# GUID and api_key are the same thing
+api_key="YOUR_API_KEY_OR_INSTALL_GUID"
+
+# Install ID from studer-innotec portal
+install_id="YOUR_INSTALL_ID"
---
+++ py_api_bat_volt.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import hashlib
+import io
+
+with open('creds.py') as f: exec(f.read())
+
+# this the reference for getting battery voltage
+info_id="3092"
+
+uhash = hashlib.sha256(email_address.encode('utf-8')).hexdigest()
+phash = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+my_headers = {
+"Content-Type": "text/html",
+"UHASH": uhash,
+"PHASH": phash
+}
+
+headers=my_headers
+
+api_call="https://api.studer-innotec.com/api/v1/installation/user-info/%s?device=XT_Group&infoId=%s&api_key=%s" % (install_id,info_id,api_key)
+
+#keep commented unless debugging
+#print (api_call)
+
+resp = requests.get(api_call,headers=headers)
+resp_dict = resp.json()
+request_status = resp_dict['status']
+battery_volt = resp_dict['floatValue']
+
+if (resp_dict['status']=="OK"):
+    print(battery_volt)
+else:
+    print("Error status: %s" % (resp_dict['status']))
