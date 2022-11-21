92986471bbc8a942dff54f95194c025837e48b53
Neil M <geek4unix@gmail.com>
2022-11-21 00:22:32 +0000

    added my error handling

--- py_api_bat_soc.py
+++ py_api_bat_soc.py
@@ -2,12 +2,14 @@ import requests
 import json
 import hashlib
 import io
+import sys
 
 with open('creds.py') as f: exec(f.read())
 
 # this the reference for getting SOC
 info_id="7032"
 
+errors="check creds.py and / or internet connection"
 
 uhash = hashlib.sha256(email_address.encode('utf-8')).hexdigest()
 phash = hashlib.md5(password.encode('utf-8')).hexdigest()
@@ -22,13 +24,20 @@ api_call="https://api.studer-innotec.com/api/v1/installation/user-info/%s?device
 
 #keep commented unless debugging
 #print (api_call)
-
-resp = requests.get(api_call,headers=headers)
-resp_dict = resp.json()
-
+try:
+  resp = requests.get(api_call,headers=headers)
+  resp_dict = resp.json()
+except:
+  print(errors)
+  sys.exit(1)
 try:
   request_status = resp_dict['status']
   battery_soc = resp_dict['floatValue']
   print(battery_soc)
 except:
-  print("Check creds file / connection to API")
+  print(errors)
+  sys.exit(1)
+
+if(battery_soc=="0.0"):
+  print(errors)
+  sys.exit(1)
--- py_api_bat_volt.py
+++ py_api_bat_volt.py
@@ -2,34 +2,42 @@ import requests
 import json
 import hashlib
 import io
+import sys
 
 with open('creds.py') as f: exec(f.read())
 
-# this the reference for getting battery voltage
+# this the reference for getting SOC
 info_id="3092"
+
+errors="check creds.py and / or internet connection"
 
 uhash = hashlib.sha256(email_address.encode('utf-8')).hexdigest()
 phash = hashlib.md5(password.encode('utf-8')).hexdigest()
 
-my_headers = {
+headers = {
 "Content-Type": "text/html",
 "UHASH": uhash,
 "PHASH": phash
 }
 
-headers=my_headers
-
 api_call="https://api.studer-innotec.com/api/v1/installation/user-info/%s?device=XT_Group&infoId=%s&api_key=%s" % (install_id,info_id,api_key)
 
 #keep commented unless debugging
 #print (api_call)
-
-resp = requests.get(api_call,headers=headers)
-resp_dict = resp.json()
-
+try:
+  resp = requests.get(api_call,headers=headers)
+  resp_dict = resp.json()
+except:
+  print(errors)
+  sys.exit(1)
 try:
   request_status = resp_dict['status']
   battery_volt = resp_dict['floatValue']
   print(battery_volt)
 except:
-  print("Check creds file / connection to API")
+  print(errors)
+  sys.exit(1)
+
+if(battery_volt=="0.0"):
+  print(errors)
+  sys.exit(1)
